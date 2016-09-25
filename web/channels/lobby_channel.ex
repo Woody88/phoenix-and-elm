1c4ff0b67ec68ea40bba9e611b9892a46a2e7460
Ricardo García Vega <bigardone@gmail.com>
2016-09-25 19:31:34 +0200

    Added lobby channel

---
+++ web/channels/lobby_channel.ex
@@ -0,0 +1,30 @@
+defmodule PhoenixAndElm.LobbyChannel do
+  use Phoenix.Channel
+  alias PhoenixAndElm.{Contact, Repo}
+  import Ecto.Query
+
+  def join("lobby", _, socket), do: {:ok, socket}
+
+  def handle_in("contacts", params, socket) do
+    search = Map.get(params, "search") || ""
+
+    page = Contact
+      |> Contact.search(search)
+      |> Ecto.Query.order_by(:first_name)
+      |> Repo.paginate(params)
+
+    {:reply, {:ok, Map.put(page, :search, search)}, socket}
+  end
+
+  def handle_in("contact:" <> contact_id, _, socket) do
+    contact = Contact
+    |> Repo.get(contact_id)
+
+    case contact do
+      nil ->
+        {:reply, {:error, %{reason: "Contact no found"}}, socket}
+      _ ->
+        {:reply, {:ok, %{contact: contact}}, socket}
+    end
+  end
+end
